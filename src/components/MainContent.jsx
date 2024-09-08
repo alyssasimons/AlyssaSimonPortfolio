02f3c19be78f7601c854007942b00471b7c0a5bc
Alyssa Simon <alysimon519@gmail.com>
2024-09-08 17:13:28 -0400

    add landing page bg

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,7 +7,7 @@ export default function MainContent() {
                 <h1> Alyssa Simon </h1>
                 <h3> Aspiring Full-Stack Developer 💻</h3>
                 <div className="links"> 
-                    <a href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+                    <a className="linkedin" href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
                     <a className="email" href="mailto:alyssasimon519@gmail.com"><i class="fa-solid fa-envelope"></i> Email</a>
                 </div>
                 <a href="#about"><i class="fa-solid fa-angles-down arrow"></i></a> 
