295a97c1ae2edf71153a829a5644d5eb26a5d9f1
Alyssa Simon <alysimon519@gmail.com>
2024-09-13 15:40:43 -0400

    remove emoji

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,7 +5,7 @@ export default function MainContent() {
         <section className="main-body" id="main">
             <div className="main-text">
                 <h1> Alyssa Simon </h1>
-                <h3> Full-Stack Web Developer 💻</h3>
+                <h3> Full-Stack Web Developer </h3>
                 <div className="links"> 
                     <a className="linkedin" href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
                     <a className="email" href="mailto:alyssasimon519@gmail.com"><i class="fa-solid fa-envelope"></i> Email</a>
