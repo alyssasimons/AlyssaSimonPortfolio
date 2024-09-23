eb79788f0f47714bfed2739d5d7dca91cd7149d3
Alyssa Simon <alysimon519@gmail.com>
2024-09-23 16:41:07 -0400

    comment out demo link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export default function Projects(props) {
             <p>{props.languages}</p>
             <div className="projectlinks">
                 <a href={props.github} target="_blank"><i class="fa-brands fa-github"></i></a>
-                <a href="https://colab.research.google.com/drive/1j0w3Quqtt462WUjA42wt26E8YWwg3pAU" target="_blank"><i class="fa-solid fa-rocket"></i></a>
+                {/*<a href="https://colab.research.google.com/drive/1j0w3Quqtt462WUjA42wt26E8YWwg3pAU" target="_blank"><i class="fa-solid fa-rocket"></i></a>*/}
             </div>
         </div>
     )
