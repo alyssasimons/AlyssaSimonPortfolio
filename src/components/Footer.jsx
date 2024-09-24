3278876350197c0116310e0064355f91ca68a3ed
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 11:17:57 -0400

    add contact links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <footer>
+        <footer id="contact">
             <p> Built and designed with ♡ by <a href="https://www.linkedin.com/in/alyssasimon" target="_blank">Alyssa Simon</a></p>
+            <div className="footerlinks">
+                <a href="https://www.linkedin/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin footericon"></i></a>
+                <a href="https://github.com/alyssasimons" target="_blank"><i class="fa-brands fa-github footericon"></i></a>
+                <a mailto="alyssasimons519@gmail.com"><i class="fa-solid fa-envelope footericon"></i></a>
+            </div>
         </footer>
     )
 }
