4edca4bebd66b30f6dd079a7f3594f4c2d59529c
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 16:03:51 -0400

    change class attributes to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
         <footer id="contact">
             <p> Built and designed with ♡ by <a href="https://www.linkedin.com/in/alyssasimon" target="_blank">Alyssa Simon</a></p>
             <div className="footerlinks">
-                <a href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin footericon"></i></a>
-                <a href="https://github.com/alyssasimons" target="_blank"><i class="fa-brands fa-github footericon"></i></a>
-                <a href="mailto:alyssasimons519@gmail.com"><i class="fa-solid fa-envelope footericon"></i></a>
+                <a href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i className="fa-brands fa-linkedin footericon"></i></a>
+                <a href="https://github.com/alyssasimons" target="_blank"><i className="fa-brands fa-github footericon"></i></a>
+                <a href="mailto:alyssasimons519@gmail.com"><i className="fa-solid fa-envelope footericon"></i></a>
             </div>
         </footer>
     )
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,10 +7,10 @@ export default function MainContent() {
                 <h1> Alyssa Simon </h1>
                 <h3> Full-Stack Web Developer </h3>
                 <div className="links"> 
-                    <a className="linkedin" href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
-                    <a className="email" href="mailto:alyssasimon519@gmail.com"><i class="fa-solid fa-envelope"></i> Email</a>
+                    <a className="linkedin" href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i className="fa-brands fa-linkedin"></i> LinkedIn</a>
+                    <a className="email" href="mailto:alyssasimon519@gmail.com"><i className="fa-solid fa-envelope"></i> Email</a>
                 </div>
-                <a href="#about"><i class="fa-solid fa-angles-down arrow"></i></a> 
+                <a href="#about"><i className="fa-solid fa-angles-down arrow"></i></a> 
             </div>
         </section>
     )
