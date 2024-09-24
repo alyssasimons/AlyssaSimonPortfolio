56a979d2eb52f5b83f5dbfd3ebf842faab83d9db
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 11:18:26 -0400

    add contact navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ export default function Header() {
                 <a href="#main">Home</a>
                 <a href="#about">About</a>
                 <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
             </nav>
         </header>
     )
