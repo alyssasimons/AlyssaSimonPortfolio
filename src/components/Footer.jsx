5a45041803f82e89de6c90654f00dfbec63d2004
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 14:26:55 -0400

    fix email link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="footerlinks">
                 <a href="https://www.linkedin.com/in/alyssasimon" target="_blank"><i class="fa-brands fa-linkedin footericon"></i></a>
                 <a href="https://github.com/alyssasimons" target="_blank"><i class="fa-brands fa-github footericon"></i></a>
-                <a mailto="alyssasimons519@gmail.com"><i class="fa-solid fa-envelope footericon"></i></a>
+                <a href="mailto:alyssasimons519@gmail.com"><i class="fa-solid fa-envelope footericon"></i></a>
             </div>
         </footer>
     )
