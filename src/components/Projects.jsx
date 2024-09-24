827bc09c18aaf80d4870c9e92a438933e2cb8f70
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 16:06:04 -0400

    change class attribute to className

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,12 +4,12 @@ export default function Projects(props) {
     return (
         <div className="project-card">
             <img className="projectimg" src={props.img} alt="Detect AI Project"/>
-            <p class="projecttitle">{props.title}</p>
+            <p className="projecttitle">{props.title}</p>
             <p>{props.description}</p>
             <p>{props.languages}</p>
             <div className="projectlinks">
-                <a href={props.github} target="_blank"><i class="fa-brands fa-github"></i></a>
-                {/*<a href="https://colab.research.google.com/drive/1j0w3Quqtt462WUjA42wt26E8YWwg3pAU" target="_blank"><i class="fa-solid fa-rocket"></i></a>*/}
+                <a href={props.github} target="_blank"><i className="fa-brands fa-github"></i></a>
+                {/*<a href="https://colab.research.google.com/drive/1j0w3Quqtt462WUjA42wt26E8YWwg3pAU" target="_blank"><i className="fa-solid fa-rocket"></i></a>*/}
             </div>
         </div>
     )
