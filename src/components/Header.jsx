1e866685f8cf32dd7fe9fed20ef0c808dd3a92af
Alyssa Simon <alysimon519@gmail.com>
2024-09-24 11:38:35 -0400

    add container for logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <img src={logo} alt="Portfolio logo containing the letters A and S beside each other"/>
+                <img src={logo} className="logoImg" alt="Portfolio logo containing the letters A and S beside each other"/>
             </div>
             <nav className="navlinks">
                 <a href="#main">Home</a>
